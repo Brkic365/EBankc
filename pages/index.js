3ff5bd5b94b8a7d68c8c4b06a145bc7936cd694b
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-01-23 16:21:35 +0100

    Fixed link error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Asset from "../components/Asset";
 import styles from "../styles/Home.module.scss";
 
