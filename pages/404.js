82df44e91175a6cf8c462991694d6d5eb33ad38f
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-01-24 23:15:30 +0100

    APOSTROPHE BUG!

--- pages/404.js
+++ pages/404.js
@@ -10,8 +10,8 @@ function NotFoundPage() {
     <div className={styles.notFound}>
       <img src="/images/404/server_down.svg" alt="Icon of server error" />
       <div className={styles.content}>
-        <h1>Something's wrong</h1>
-        <p>We can't seem to find the page you are looking for..</p>
+        <h1>{"Something's"} wrong</h1>
+        <p>We {"can't"} seem to find the page you are looking for..</p>
         <button onClick={() => router.back()}>Go back</button>
       </div>
     </div>
