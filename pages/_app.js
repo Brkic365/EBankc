2a99b56ba9bbeb3e4c8dcd2fab1ed24510ce81ff
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2022-01-31 18:41:38 +0100

    Next-SEO added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -14,17 +15,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <meta
-          property="og:image"
-          content="https://ebankc.netlify.app/images/meta_image.png"
-        />
-        <meta
-          property="twitter:image"
-          content="https://ebankc.netlify.app/images/meta_image.png"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo
+        defaultTitle="EBANKC"
+        description="Unlocking the power of Defi-Banking | Earn Interest, borrow, and exchange digital assets at competitive rates"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "EBankc",
+          description:
+            "Unlocking the power of Defi-Banking | Earn Interest, borrow, and exchange digital assets at competitive rates",
+          images: [
+            {
+              url: "https://www.ebankc.netlify.app/images/meta_image.png",
+            },
+          ],
+          site_name: "EBankc",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
       <Navbar />
       <Component {...pageProps} />
       {!noFooter.includes(router.pathname) && <Footer />}
